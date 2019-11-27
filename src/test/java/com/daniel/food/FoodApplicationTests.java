401f943d21260c52c176dc92ec0e75d0ab364642
Daniel <daniel.d.ilievski@gmail.com>
2019-11-27 02:26:15 +0100

    2

--- src/test/java/com/daniel/food/FoodApplicationTests.java
+++ src/test/java/com/daniel/food/FoodApplicationTests.java
@@ -1,13 +1,18 @@
 package com.daniel.food;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FoodApplicationTests {
+	
+	RecipeService recipeService = new RecipeService();
 
 	@Test
-	void contextLoads() {
+	void listRecipes() {
+		
 	}
 
 }
